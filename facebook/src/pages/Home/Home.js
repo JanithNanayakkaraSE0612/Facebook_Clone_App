caaeac1c7349d1c2b7fa3c18921977ced7145b62
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-11 03:55:55 +0530

    Today Over

--- facebook/src/pages/Home/Home.js
+++ facebook/src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../../components/Navigation/NavBar'
+import LeftPane from '../../components/LeftPane/LeftPane'
 
 export default function Home() {
   return (
     <div>
       <NavBar/>
+      <LeftPane/>
     </div>
   )
 }

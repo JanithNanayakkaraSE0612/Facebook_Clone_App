cb3c0ac802a21725f8449980245517adde2505ac
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-11 01:44:09 +0530

    Icon added

--- facebook/src/components/Navigation/NavBar.js
+++ facebook/src/components/Navigation/NavBar.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function NavBar() {
   return (
@@ -8,10 +12,27 @@ export default function NavBar() {
         </div>
         <div className="navBarCenter">
             <div className="searchBarBox">
-                
+                <SearchIcon className='searchIcon'/>
+                <input placeholder='search for your friend' className='searchInput' />
             </div>
         </div>
-        <div className="navBarRight"></div>
+        <div className="navBarRight">
+            <div className="navBarLinks">
+                <span className='navBarLink'>Home</span>
+                <span className='navBarLink'>Profile</span>
+            </div>
+            <div className="navBarIcons">
+                <div className="navBarIcon">
+                    <PersonIcon/>
+                </div>
+                <div className="navBarIcon">
+                    <MessageIcon/>
+                </div>
+                <div className="navBarIcon">
+                    <SettingsIcon/>
+                </div>
+            </div>
+        </div>
     </div> 
   )
 }
--- facebook/src/pages/Home/Home.js
+++ facebook/src/pages/Home/Home.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div>
       <NavBar/>
-      This is Home Page
     </div>
   )
 }

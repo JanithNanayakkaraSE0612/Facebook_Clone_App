a513395604ab295b0afcbbe9d9e21b399b6d177d
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-13 00:19:04 +0530

    Added Resource Images

--- facebook/src/components/Post/Post.js
+++ facebook/src/components/Post/Post.js
@@ -16,12 +16,16 @@ export default function Post() {
             </div>
             <div className="postBottom">
                 <div className="postBottomleft">
-                    <img src="" alt="" className="readctionPic" />
-                    <img src="" alt="" className="readctionPic" />
-                    <img src="" alt="" className="readctionPic" />
+                    <img src="/images/like.png" alt="" className="readctionPic" />
+                    <img src="/images/heart.png" alt="" className="readctionPic" />
+                    <img src="/images/haha.png" alt="" className="readctionPic" />
                     <span className="likeCount">Batman and 225 other</span>
                 </div>
-                    <div className="postBottomRight"></div>
+                    <div className="postBottomRight">
+                        <span className="commentCount">
+                            15 Comments
+                        </span>
+                    </div>
             </div>
        </div>
     </div>

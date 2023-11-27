d26cab61ae9ab48ea8c07a3f30dd201a732de73c
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2023-11-28 02:50:10 +0530

    Update

--- facebook/src/components/LeftPane/LeftPane.js
+++ facebook/src/components/LeftPane/LeftPane.js
@@ -89,18 +89,6 @@ export default function LeftPane() {
             <img src="/images/newgenius.png" alt="" className="pagePic" />
               <span className="pageName">newGeniusTechnology</span>
           </li>
-          <li className="pageListItem">
-            <img src="/images/newgenius.png" alt="" className="pagePic" />
-              <span className="pageName">newGeniusTechnology</span>
-          </li>
-          <li className="pageListItem">
-            <img src="/images/newgenius.png" alt="" className="pagePic" />
-              <span className="pageName">newGeniusTechnology</span>
-          </li>
-          <li className="pageListItem">
-            <img src="/images/newgenius.png" alt="" className="pagePic" />
-              <span className="pageName">newGeniusTechnology</span>
-          </li>
             </div>
         </div>
       </div>

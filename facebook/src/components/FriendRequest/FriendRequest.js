f5145ddcd967e4018523c42b2596664769710523
JanithNanayakkaraSE0612 <JanithNanayakkara0612@outlook.com>
2024-02-12 21:50:53 +0530

    added

--- facebook/src/components/FriendRequest/FriendRequest.js
+++ facebook/src/components/FriendRequest/FriendRequest.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function FriendRequest() {
   return (
     <div className='w-24 h-40'>
-        
+        <div>
+          
+        </div>
     </div>
   )
 }
